3312817b154c29616b6ae7d64ad371988bbdabbb
Robin Jessome <robinjessome@gmail.com>
2026-04-05 12:20:24 -0400

    init

---
+++ sanity/schemaTypes/postType.ts
@@ -0,0 +1,53 @@
+import {DocumentTextIcon} from '@sanity/icons'
+import {defineField, defineType} from 'sanity'
+
+export const postType = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  icon: DocumentTextIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      name: 'mainImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        defineField({
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        })
+      ]
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      type: 'blockContent',
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      return {...selection}
+    },
+  },
+})
